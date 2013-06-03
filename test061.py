3ec14f2a3bdf733a9b21bc66cd53172ab152d4aa
Wolf Halton <wolf@sourcefreedom.com>
2013-06-03 12:23:34 -0400

    Work on test06.py and test061.py

--- test061.py
+++ test061.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#  untitled.py
+#  test061.py
 #  
-#  Copyright 2013 mhalton <mhalton@mint-141>
+#  Copyright 2013 Wolf Halton <wolf@sourcefreedom.com>
 #  
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
